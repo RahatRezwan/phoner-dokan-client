eee7d24ab8b73dfd1c719a5e437b782aa8fd3f11
Kazi Rahat <kazirahat1007@outlook.com>
2022-11-28 23:07:38 +0600

    fix few more error

--- src/routes/AdminRoute/AdminRoute.jsx
+++ src/routes/AdminRoute/AdminRoute.jsx
@@ -14,7 +14,7 @@ const AdminRoute = ({ children }) => {
    if (user && isAdmin) {
       return children;
    }
-   return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
+   return <Navigate to="/dashboard" state={{ from: location }} replace></Navigate>;
 };
 
 export default AdminRoute;
