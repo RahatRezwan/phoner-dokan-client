2f4b462230b0fe34ebc3294a54abe6e49eaae42c
Kazi Rahat <kazirahat1007@outlook.com>
2022-11-28 15:10:11 +0600

    separate admin, buyer and seller route in dashboard page

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -35,89 +35,96 @@ const DashboardLayout = () => {
          .catch((err) => console.log(err));
    };
    return (
-      <div className="flex flex-col md:flex-row min-h-screen max-w-[1440px]">
-         <div className="min-h-full p-5 shadow-md flex flex-row md:flex-col items-center justify-center md:justify-start gap-10 md:mt-7">
-            <button
-               htmlFor="dashboard-drawer"
-               onClick={() => setShow(!show)}
-               className="drawer-button cursor-pointer tooltip tooltip-bottom md:tooltip-right"
-               data-tip="Open/Close SideBar"
-            >
-               <RiMenuUnfoldLine className="text-3xl" />
-            </button>
-            <Link
-               to="/"
-               className="text-3xl tooltip tooltip-bottom md:tooltip-right"
-               data-tip="Go to Home"
-            >
-               <FaHome />
-            </Link>
-            <Link
-               to="/dashboard"
-               className="text-3xl tooltip tooltip-bottom md:tooltip-right"
-               data-tip="Go to Dashboard"
-            >
-               <FaUserAlt />
-            </Link>
-            <Link
-               onClick={handleLogout}
-               className="text-3xl tooltip tooltip-bottom md:tooltip-right"
-               data-tip="Logout"
-            >
-               <FiLogOut />
-            </Link>
-         </div>
-         <div className="flex gap-5 w-full">
-            <div className={show ? "block  min-h-screen w-[40%] lg:w-[20%]" : "hidden"}>
-               <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
-               <ul className="menu p-4 bg-base-100 text-base-content pt-10 w-full h-full">
-                  <li>
-                     <Link to="/">Go to Home</Link>
-                  </li>
-                  <li>
-                     <Link to="/dashboard">Dashboard</Link>
-                  </li>
-                  <li>
-                     <Link to="/dashboard/my-orders">My Orders</Link>
-                  </li>
-                  <li>
-                     <Link to="/dashboard/my-wishlist">My Wishlist</Link>
-                  </li>
-
-                  {isAdmin ? (
-                     <>
-                        {/* Admin Route */}
-                        {adminMenus.map((menu) => (
-                           <li key={menu.id}>
-                              <Link to={menu.route}>{menu.name}</Link>
-                           </li>
-                        ))}
-                     </>
-                  ) : null}
-
-                  {isSeller ? (
-                     <>
-                        {/* Seller Routes */}
-                        {sellerMenus.map((menu) => (
-                           <li key={menu.id}>
-                              <Link to={menu.route}>{menu.name}</Link>
-                           </li>
-                        ))}
-                     </>
-                  ) : null}
-
-                  <li>
-                     <Link onClick={handleLogout}>
-                        Logout <FiLogOut />
-                     </Link>
-                  </li>
-               </ul>
+      <>
+         <div className="flex flex-col md:flex-row min-h-screen max-w-[1440px]">
+            <div className="min-h-full p-5 shadow-md flex flex-row md:flex-col items-center justify-center md:justify-start gap-10 md:mt-7">
+               <button
+                  htmlFor="dashboard-drawer"
+                  onClick={() => setShow(!show)}
+                  className="drawer-button cursor-pointer tooltip tooltip-bottom md:tooltip-right"
+                  data-tip="Open/Close SideBar"
+               >
+                  <RiMenuUnfoldLine className="text-3xl" />
+               </button>
+               <Link
+                  to="/"
+                  className="text-3xl tooltip tooltip-bottom md:tooltip-right"
+                  data-tip="Go to Home"
+               >
+                  <FaHome />
+               </Link>
+               <Link
+                  to="/dashboard"
+                  className="text-3xl tooltip tooltip-bottom md:tooltip-right"
+                  data-tip="Go to Dashboard"
+               >
+                  <FaUserAlt />
+               </Link>
+               <Link
+                  onClick={handleLogout}
+                  className="text-3xl tooltip tooltip-bottom md:tooltip-right"
+                  data-tip="Logout"
+               >
+                  <FiLogOut />
+               </Link>
             </div>
-            <div className="drawer-content py-10 px-5 w-full">
-               <Outlet />
+            <div className="flex gap-5 w-full">
+               <div className={show ? "block  min-h-screen w-[40%] lg:w-[20%]" : "hidden"}>
+                  <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
+                  <ul className="menu p-4 bg-base-100 text-base-content pt-10 w-full h-full">
+                     <li>
+                        <Link to="/">Go to Home</Link>
+                     </li>
+                     <li>
+                        <Link to="/dashboard">Dashboard</Link>
+                     </li>
+
+                     {isAdmin || isSeller || (
+                        <>
+                           <li>
+                              <Link to="/dashboard/my-orders">My Orders</Link>
+                           </li>
+                           <li>
+                              <Link to="/dashboard/my-wishlist">My Wishlist</Link>
+                           </li>
+                        </>
+                     )}
+
+                     {isAdmin ? (
+                        <>
+                           {/* Admin Route */}
+                           {adminMenus.map((menu) => (
+                              <li key={menu.id}>
+                                 <Link to={menu.route}>{menu.name}</Link>
+                              </li>
+                           ))}
+                        </>
+                     ) : null}
+
+                     {isSeller ? (
+                        <>
+                           {/* Seller Routes */}
+                           {sellerMenus.map((menu) => (
+                              <li key={menu.id}>
+                                 <Link to={menu.route}>{menu.name}</Link>
+                              </li>
+                           ))}
+                        </>
+                     ) : null}
+
+                     <li>
+                        <Link onClick={handleLogout}>
+                           Logout <FiLogOut />
+                        </Link>
+                     </li>
+                  </ul>
+               </div>
+               <div className="drawer-content py-10 px-5 w-full">
+                  <Outlet />
+               </div>
             </div>
          </div>
-      </div>
+      </>
    );
 };
 
